0ba6a600925dab108c221b38d21aa6d07df7d4ce
Harshit <Itsharshitgoat@gmail.com>
2025-08-12 22:12:54 +0530

    Add files via upload

--- Programming In C/sample.c
+++ Programming In C/sample.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-int main()
+void main()
 {
-    int a = 1, b = 2, sum = 0;
-    sum = a + b;
-    printf("%d\n", sum);
-    return 0;
+    int a = 1, b = 2, sum = 0 , sub=0 , div=0 , mul=0;
+    sum = a + b , sub=a-b , div=a/b , mul=a*b;
+    printf("%d", sum,sub,mul,div);
 }
