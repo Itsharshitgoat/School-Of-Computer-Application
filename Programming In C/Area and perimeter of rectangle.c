d310ce06c3f3cf1290d7df34abb5b86e848c40e4
Harshit <Itsharshitgoat@gmail.com>
2025-09-15 12:26:41 +0530

    Add files via upload

---
+++ Programming In C/Area and perimeter of rectangle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+    float l=0, w=0 , p=0 , a=0;
+    scanf("%f", &l);
+    scanf("%f", &w);
+    a = l*w;
+    p = 2*(l+w);
+    printf("%f\n", a);
+    printf("%f\n", p);
+}
