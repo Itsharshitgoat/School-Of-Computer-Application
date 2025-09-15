9c378818176877f9800de2f65f3b69c2467d35de
Harshit <Itsharshitgoat@gmail.com>
2025-09-15 12:27:04 +0530

    Add files via upload

---
+++ Programming In C/Area and perimeter of triangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main() {
+    float ai=0, b=0 , c=0 , a=0 , p=0;
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
+    ai = a*b;
+    p = a+b+c;
+    printf("%f\n", ai);
+    printf("%f\n", p);
+}
