11946b018d1f202291c36b7018f8b21fadc175b2
Harshit <Itsharshitgoat@gmail.com>
2025-10-15 00:44:48 +0530

    Delete DBMS directory

--- DBMS/Inserting Values.sql
+++
@@ -1,5 +0,0 @@
-use ai;
-insert into ai_m
-value(1,'Deepseek',671),(2,'Qwen',235),(3,'GPT OSS',120),(4,'Llama',405),(5,'Phi',14);
-insert into ai_ap
-value(1,'Deepseek',37),(2,'Qwen',22),(3,'GPT OSS',20),(4,'Llama',65),(5,'Phi',4);
